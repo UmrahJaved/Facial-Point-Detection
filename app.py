f362e67e643b2705537b12e6592a60c5b9bf181c
yassbarona <yasserbarona@gmail.com>
2022-06-29 19:56:35 +0200

    step2 images exampls

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import mediapipe as mp
 import numpy as np
 from matplotlib import pyplot as plt
 import streamlit as st
+from validators import diners
+from zipfile import ZipFile
+import os
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
@@ -11,61 +14,101 @@ title = '<p style="font-family:Courier; color:Black; font-size: 60px; font-weigh
 st.markdown(title, unsafe_allow_html=True)
 st.text('Work hard... Play hard')
 
-data = st.file_uploader("Upload a video", type=None)
-
 # Face Mesh
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh()
-
-if data is not None:
-    image1 = cv2.imread('data/face.jpg')
-    rgb_image = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
-    result = face_mesh.process(rgb_image)
-    
-    height, width, _ = image1.shape
-    
-    # Facial landmarks
-    result = face_mesh.process(image1)
-    landmark_points_68 = [162,234,93,58,172,136,149,148,152,377,378,365,397,288,323,454,389,71,63,105,66,107,336,
+landmark_points_68 = [162,234,93,58,172,136,149,148,152,377,378,365,397,288,323,454,389,71,63,105,66,107,336,
                 296,334,293,301,168,197,5,4,75,97,2,326,305,33,160,158,133,153,144,362,385,387,263,373,
                 380,61,39,37,0,267,269,291,405,314,17,84,181,78,82,13,312,308,317,14,87]
-    canvas = np.zeros((image1.shape[1], image1.shape[0], 1), dtype = "uint8")
-    d_pxl = 3
-    e_indexes = []
-    
-    with st.container():
+image1 = cv2.imread('data/face.jpg')
+rgb_image = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
+result = face_mesh.process(rgb_image)    
+height, width, _ = image1.shape
+# Facial landmarks
+result = face_mesh.process(image1)
+#Black background
+canvas = np.zeros((image1.shape[1], image1.shape[0], 1), dtype = "uint8")
 
-        col1,col2 = st.columns(2)
+def num_face(pxl=3, indexes=landmark_points_68):
+    for facial_landmarks in result.multi_face_landmarks:
+        for i in indexes :
+            pt1 = facial_landmarks.landmark[i]
+            x = int(pt1.x * width)
+            y = int(pt1.y * height) 
+            cv2.putText(canvas, str(i), (x,y), 0, 0.3, (255,255,255))             
+    plt.imshow(canvas)
+    fig = plt.show()
+    st.pyplot(fig)
+
+def dot_face(pxl=3, indexes=landmark_points_68):
+    for facial_landmarks in result.multi_face_landmarks:
+        for i in indexes :
+            pt1 = facial_landmarks.landmark[i]
+            x = int(pt1.x * width)
+            y = int(pt1.y * height) 
+            cv2.circle(canvas, (x, y), pxl, (100, 100, 0), -1)            
+    plt.imshow(canvas)
+    fig = plt.show()
+    st.pyplot(fig)
+
+st.subheader('Step 1: Upload video')
+data = st.file_uploader("Upload a video", type=None, accept_multiple_files=True)
+
+if data is not None:
+
+    with st.container():
+        st.subheader('List of facial indexes')
+        st.image('./data/image3.png')
+
+
+    with st.container():
+        st.subheader('Step 2: Set parameters and preview results')
+        pixel = st.text_input('Pixel size as integer , eg: 3')
+        i_drop_str = st.text_input('List of comma separated indexes, eg: 1,12,23,34,45...')
+
+    with st.container():
+        col1, col2, col3 = st.columns(3) 
 
         with col1:
-            st.image(rgb_image)
-            
-        with col2:
+            reset = st.button('Reset')
+                
 
-            for facial_landmarks in result.multi_face_landmarks:
-                for i in landmark_points_68 :
-                    pt1 = facial_landmarks.landmark[i]
-                    x = int(pt1.x * width)
-                    y = int(pt1.y * height)
-                    cv2.circle(canvas, (x, y), d_pxl, (100, 100, 0), -1)                
-            plt.imshow(canvas)
-            fig = plt.show()
-            st.pyplot(fig)
-    
-    with st.container():
-        for facial_landmarks in result.multi_face_landmarks:
-            for i in landmark_points_68 :
-                pt1 = facial_landmarks.landmark[i]
-                x = int(pt1.x * width)
-                y = int(pt1.y * height) 
-                cv2.putText(canvas, str(i), (x,y), 0, 0.3, (255,255,255))             
-        plt.imshow(canvas)
-        fig = plt.show()
-        st.pyplot(fig)
+        with col2:        
+            preview =  st.button('Preview')
+        
+        with col3:
+            next = st.button('Next')
 
     with st.container():
+        col1, col2 = st.columns(2) 
+
+        if reset:
+            with col1:
+                dot_face()
+            with col2:
+                num_face()
+                
+        if preview:
+            d_indexes = landmark_points_68
+            print(d_indexes)
+            i_drop_l = i_drop_str.split(',')
+            print(i_drop_l)
+            for i in i_drop_l:
+                d_indexes.remove(int(i))
+            print(d_indexes)
+            if not int(pixel):
+                with col1:
+                    dot_face(3, d_indexes) 
+                with col2:
+                    num_face(3, d_indexes)
+            else: 
+                with col1:
+                    dot_face(int(pixel),d_indexes) 
+                with col2:
+                    num_face(int(pixel),d_indexes)
+
+        with st.container():
         video_path = f'./data/{data.name}'
-        print('video path: ', video_path)
         cap = cv2.VideoCapture(video_path)
         ret, image2 = cap.read()
         height, width, channels = image2.shape
@@ -78,7 +121,6 @@ if data is not None:
                 break
             height, width, channels = image3.shape
             # Facial landmarks
-            print('img ', image3.shape)
             result = face_mesh.process(image3)
             landmark_points_68 = [162,234,93,58,172,136,149,148,152,377,378,365,397,288,323,454,389,71,63,105,66,107,336,
                     296,334,293,301,168,197,5,4,75,97,2,326,305,33,160,158,133,153,144,362,385,387,263,373,
@@ -86,9 +128,7 @@ if data is not None:
 
             canvas = np.zeros((image3.shape[1], image3.shape[0], 1), dtype = "uint8")
             if not result.multi_face_landmarks:
-                print("landmark NOT found")
                 continue            
-            print("landmark found")
             for facial_landmarks in result.multi_face_landmarks: 
                 for i in landmark_points_68 :
                     pt1 = facial_landmarks.landmark[i]
@@ -96,6 +136,13 @@ if data is not None:
                     y = int(pt1.y * height)
                     cv2.circle(canvas, (x, y), 3, (100, 100, 0), -1)
             out.write(canvas)
-    
+
         cap.release()
         out.release()
+
+
+
+            
+
+
+
